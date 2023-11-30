11977f8ecfc4e88b980e01e61e4c46a8b114322d
Steevan BARBOYON <steevan.barboyon@gmail.com>
2023-11-30 15:16:35 +0100

    Calculate lego mosaic dimensions

---
+++ docs/calculate-image-dimensions.js
@@ -0,0 +1,17 @@
+function getImageDimensions(processedImage) {
+    let processedImageWidth = processedImage.naturalWidth * 0.8;
+    let processedImageWidthUnit = 'cm';
+    if (processedImageWidth > 100) {
+        processedImageWidth = processedImageWidth / 100;
+        processedImageWidthUnit = 'm';
+    }
+
+    let processedImageHeight = processedImage.naturalHeight * 0.8;
+    let processedImageHeightUnit = 'm';
+    if (processedImageHeight > 100) {
+        processedImageHeight = processedImageHeight / 100;
+        processedImageHeightUnit = 'm';
+    }
+
+    return `${processedImageWidth.toFixed(2)}${processedImageWidthUnit} x ${processedImageHeight.toFixed(2)}${processedImageHeightUnit}`;
+}
